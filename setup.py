bb753a5416748474209853231f7e610defb9ecc8
Bart Mosley <bartm@bondgeek.com>
2019-05-05 15:17:42 -0400

    fix errors in setup.py

--- setup.py
+++ setup.py
@@ -9,6 +9,6 @@ setup(
     license='MIT LICENSE',
     author='Bart Mosley',
     author_email='bartm@bondgeek.com',
-    description='Utilities for handling CUSIP check digits.'
+    description='Utilities for handling CUSIP check digits.',
     url='https://github.com/bondgeek/bgqsp',
 )
